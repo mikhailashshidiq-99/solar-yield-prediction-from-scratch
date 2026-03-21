d968d744045baebccf5f459ccf9614f886e35604
Mikhail Arumbinang <mikhailashshidiqmaulanaarumbinang@mail.ugm.ac.id>
2026-03-21 17:05:41 +0700

    feat(math): Added the Z-standardization module for preprocessing data

---
+++ src/preprocessing.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Z-Standardization
+# Z = (X - mu)/sigma
+
+def Z_Standardization(df):
+    y_df = df['expected_power_output']
+    X_df = df.drop(columns=['expected_power_output'])
+
+    y = y_df.to_numpy()
+    X = X_df.to_numpy()
+
+    mu = np.mean(X, axis=0)
+    sigma = np.std(X, axis=0)
+
+    # Avoid division by zero
+    # If any value = 0, this replaces it with 1e-8
+    sigma = np.where(sigma == 0, 1e-8, sigma)
+
+    X_scaled = (X-mu) / sigma
+
+    return X_scaled, y, mu, sigma
+
