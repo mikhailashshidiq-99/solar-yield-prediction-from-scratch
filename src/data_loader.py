d8987cbe89c6ad264d9db946c9c816117e829c50
Mikhail Arumbinang <mikhailashshidiqmaulanaarumbinang@mail.ugm.ac.id>
2026-03-21 16:05:57 +0700

    feat(data): fetched data using API => converted the linear data into cyclical data

---
+++ src/data_loader.py
@@ -0,0 +1,38 @@
+import pandas as pandas
+import numpy as np 
+from src.secrets import NREL_API_KEY, NREL_EMAIL
+
+def fetch_and_encode_nsrdb_data(lat, lon, year):
+    print(f"Fetching NSRDB data for Lat: {lat}, Lon: {lon}, Year: {year}...")
+
+    attributes = "ghi,dhi,dni,wind_speed,air_temperature,relative_humidity"
+
+    url = (
+        f"https://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?"
+        f"wkt=POINT({lon}%20{lat})&names={year}&leap_day=false&interval=60&utc=false"
+        f"&full_name=Bare+Metal+Dev&email={NREL_EMAIL}&affiliation=Private"
+        f"&mailing_list=false&reason=academic&api_key={NREL_API_KEY}&attributes={attributes}"
+    )
+
+    # API returns Year, Month, Day, Hour, Minute.
+
+    df['datetime'] = pd.to_datetime(df[['Year', 'Month', 'Day', 'Hour', 'Minute']])
+    df['day_of_year'] = df['datetime'].dt.dayofyear
+
+    # Transform linear time into unit circle
+    df['hour_sin'] = np.sin(2 * np.pi * df['Hour']/24.0)
+    df['hour_cos'] = np.cos(2 * np.pi * df['Hour']/24.0)
+
+    df['day_sin'] = np.sin(2 * np.pi * df['day_of_year'] / 365.0)
+    df['day_cos'] = np.cos(2 * np.pi * df['day_of_year'] / 365.0)
+
+    # Drop the raw linear time columns to isolate feature matrix
+    df = df.drop(columns=['Year', 'Month', 'Day', 'Hour', 'Minute', 'datetime', 'day_of_year'])
+
+    df = df.rename(columns={
+        'air_temperature': 'temperature',
+        'ghi': 'expected_power_output'
+    })
+
+    print("API data successfully fetched and temporally encoded.")
+    return df
