e2e5d264c9827d93975dc271c2350b48c4423e3a
Mikhail Arumbinang <mikhailashshidiqmaulanaarumbinang@mail.ugm.ac.id>
2026-03-25 12:12:03 +0700

    refact(math): Transformed all the functions into a Class in for the model.

--- src/model.py
+++ src/model.py
@@ -1,39 +1,41 @@
 import numpy as np
 
-def compute_mse(X, y, w):
+class CustomLinearRegression:
+    def __init__(self, learning_rate=0.01, epochs = 1000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.loss_history = []
 
-    predictions = np.dot(X, w)
-    errors = predictions - y
-    mse = np.mean(np.square(errors))
-    return mse
-
-def compute_gradient(X, y, w):
-
-    N = X.shape[0]
-    predictions = np.dot(X, w)
-    errors = predictions - y
-
-    gradient = (2/N) * np.dot(X.T, errors)
-
-    return gradient
-
-def train_linear_regression(X, y, learning_rate=0.01, epochs=1000):
-
-    n_features = X.shape[1]
-    w = np.zeros(n_features)
-
-    loss_history = []
-
-    for i in range(epochs):
-        gradient = compute_gradient(X, y, w)
-
-        w = w - (learning_rate * gradient)
-
-        current_loss = compute_mse(X, y, w)
-        loss_history.append(current_loss)
-
-        if i % 100 == 0:
-            print(f"Epoch {i:4d} | MSE Loss: {current_loss:.4f}")
+    def _compute_gradient(self, X, y):
+        N = X.shape[0]
+        predictions = np.dot(X, self.weights)
+        errors = predictions - y
+        gradient = (2 / N) * np.dot(X.T, errors)
+        return gradient
     
-    print(f"Training complete. Final MSE Loss: {loss_history[-1]:.4f}")
-    return w, loss_history
+    def _compute_mse(self, X,y):
+        predictions = np.dot(X, self.weights)
+        errors = predictions - y
+        return np.mean(np.square(errors))
+
+    def fit(self, X, y):
+        n_features = X.shape[1]
+        self.weights = np.zeros(n_features)
+        self.loss_history = []
+
+        for i in range(slef.epochs):
+            gradient = self._compute_gradient(X, y)
+            self.weights -= (self.learning_rate * gradient)
+            self.loss_history.append(self._compute_mse(X, y))
+
+            if i % 100 == 0:
+                print(f"Epoch {i:4d} | MSE Loss: {self.loss_history[-1]:.4f}")
+                
+        return self
+
+    def predict(self, X):
+        if self.weights is None:
+            raise ValueError("Model DNE: Call .fit() before .predict()")
+
+        return np.dot(X, self.weights)
